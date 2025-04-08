486164c2b576924929f1d13935f0f929c11a7ecd
YJLin <yongjielin@ch-finance.com.tw>
2025-04-08 09:48:17 +0800

    update

--- DisableWinEco/MainWindow.xaml.cs
+++ DisableWinEco/MainWindow.xaml.cs
@@ -13,27 +13,31 @@ namespace DisableWinEco
     public partial class MainWindow : System.Windows.Window
     {
         private string[] targerProcessNames;
-        private NotifyIcon trayIcon;
+        private NotifyIcon trayIcon = new();
         private bool isRunning = true;
-        private CancellationTokenSource cts = new CancellationTokenSource();
-        private string startupFolder;
-        private string shortcutPath;
-        private string processListFilePath;
+        private CancellationTokenSource cts = new();
+        private readonly string startupFolder = "";
+        private readonly string shortcutPath = "";
+        private readonly string processListFilePath = "";
 
         public MainWindow()
         {
             InitializeComponent();
-            initialTaryIcon();
 
-            string workingDictPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-            processListFilePath = Path.Combine(workingDictPath, "processlist.txt");
+            InitialTaryIcon();
+
+            string? workingDictPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            if (string.IsNullOrEmpty(workingDictPath) == false)
+            {
+                processListFilePath = Path.Combine(workingDictPath, "processlist.txt");
+            }
 
             LoadListFromTemp();
 
             targerProcessNames = myTextBox.Text.Split(',');
 
             SwitchButtonText();
-            
+
             Task taskA = Task.Run(() => ScheduledCheck(), cts.Token);
 
             startupFolder = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
@@ -48,7 +52,7 @@ namespace DisableWinEco
                 myCheckBox.IsChecked = false;
             }
         }
-   
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             isRunning = !isRunning;
@@ -83,7 +87,7 @@ namespace DisableWinEco
         {
             while (isRunning)
             {
-                CheckProcesssProirity(); 
+                CheckProcesssProirity();
                 System.Threading.Thread.Sleep(60000);
             }
         }
@@ -119,7 +123,7 @@ namespace DisableWinEco
         }
 
         #region taryIconAndCloseEvent
-        private void initialTaryIcon()
+        private void InitialTaryIcon()
         {
             trayIcon = new NotifyIcon
             {
@@ -127,26 +131,25 @@ namespace DisableWinEco
                 Visible = true
             };
 
-
-            ContextMenuStrip menu = new ContextMenuStrip();
+            ContextMenuStrip menu = new();
             menu.Items.Add("Show", null, ShowApp);
             menu.Items.Add("Exit", null, ExitApp);
 
             trayIcon.ContextMenuStrip = menu;
         }
-        private void ShowApp(object sender, EventArgs e)
+        private void ShowApp(object? sender, EventArgs e)
         {
             this.Activate();
             this.Visibility = Visibility.Visible;
         }
 
-
-        private void ExitApp(object sender, EventArgs e)
+        private void ExitApp(object? sender, EventArgs e)
         {
             trayIcon.Visible = false;
             trayIcon.Dispose();
             Environment.Exit(0);
         }
+
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             e.Cancel = true; // 取消關閉事件
@@ -166,7 +169,7 @@ namespace DisableWinEco
             if (System.IO.File.Exists(processListFilePath))
             {
                 targerProcessNames = System.IO.File.ReadAllLines(processListFilePath);
-                myTextBox.Text = string.Join(",", targerProcessNames); 
+                myTextBox.Text = string.Join(",", targerProcessNames);
             }
             else
             {
@@ -178,7 +181,7 @@ namespace DisableWinEco
 
         #region SetProcessPriority
 
-        // 定義 Windows API 函數
+ 
         [DllImport("kernel32.dll")]
         public static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, int dwProcessId);
 
@@ -191,18 +194,18 @@ namespace DisableWinEco
         [DllImport("kernel32.dll")]
         public static extern bool CloseHandle(IntPtr hObject);
 
-        const uint PROCESS_ALL_ACCESS = 0x1F0FFF; // 完全訪問權限
-        const uint HIGH_PRIORITY_CLASS = 0x00000080; // 高優先級類別
+        const uint PROCESS_ALL_ACCESS = 0x1F0FFF; 
+        const uint HIGH_PRIORITY_CLASS = 0x00000080;  
 
         static void SetPriorityExample(IntPtr processHandle)
         {
             if (SetPriorityClass(processHandle, HIGH_PRIORITY_CLASS))
             {
-                Debug.WriteLine("成功設置進程為高優先級！");
+                Debug.WriteLine("Success");
             }
             else
             {
-                System.Windows.MessageBox.Show("設定失敗");
+                System.Windows.MessageBox.Show("Fail");
             }
         }
         #endregion
